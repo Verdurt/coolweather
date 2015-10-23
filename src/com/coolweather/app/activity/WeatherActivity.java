ed20bb1468f23597f615dabc2d203decd92ffbd6
fss <2281022112@qq.com>
2015-10-23 16:02:37 +0800

    自动更新天气

--- src/com/coolweather/app/activity/WeatherActivity.java
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.coolweather.app.activity;
 
 import com.coolweather.app.R;
+import com.coolweather.app.service.AutoUpdateService;
 import com.coolweather.app.util.HttpCallbackListener;
 import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utility;
@@ -123,6 +124,8 @@ public class WeatherActivity extends Activity {
 		publishText.setText("½ñÌì"+prefs.getString("publish_time", ""));
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
-		cityNameText.setVisibility(View.VISIBLE);		
+		cityNameText.setVisibility(View.VISIBLE);	
+		Intent intent=new Intent(this,AutoUpdateService.class);
+		startService(intent);
 	}
 }
